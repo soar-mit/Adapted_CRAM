d56938dd703ff6e12e24606c667fdc1a7e10ef3f
Dean Price <dnprice@mit.edu>
2026-06-14 00:38:37 -0400

    generalized for t ne 1

--- Adapted_CRAM.py
+++ Adapted_CRAM.py
@@ -3,7 +3,7 @@ import scipy.sparse as sp
 
 from poles_residues import *
 
-def Adapted_CRAM(q_mu: sp.csc_array, c: sp.csc_array, corr: np.ndarray, s: np.ndarray, n0: np.ndarray):
+def Adapted_CRAM(q_mu: sp.csc_array, c: sp.csc_array, corr: np.ndarray, s: np.ndarray, n0: np.ndarray, t: float):
     """
     q_mu: K X K
     c: K X K*N_x 
@@ -19,18 +19,18 @@ def Adapted_CRAM(q_mu: sp.csc_array, c: sp.csc_array, corr: np.ndarray, s: np.nd
     us = np.zeros((K, KN_x))
     dn2 = np.zeros(K)
     for alpha, theta in zip(c48_alpha, c48_theta):
-        luobj = sp.linalg.splu(q_mu - theta*iden)
+        luobj = sp.linalg.splu(t*q_mu - theta*iden)
         y0 = luobj.solve(np0)
         np0 += 2 * np.real(alpha * y0)
         dn2_shift = np.zeros(K, dtype = np.complex128) #accumulator for dn2 shift
         for kn_x in range(KN_x):
             col = s[kn_x]*c[:,[kn_x]]
             iso_index = kn_x % K
-            yn = luobj.solve(us[:, kn_x] - s[kn_x]*c[:,kn_x].toarray().ravel()*y0[iso_index])
+            yn = luobj.solve(us[:, kn_x] - t*s[kn_x]*c[:,kn_x].toarray().ravel()*y0[iso_index])
             us[:, kn_x] += 2 * np.real(alpha * yn)
 
             for kn_x2 in range(KN_x):
-                dn2_shift -= corr[kn_x, kn_x2]*s[kn_x2]*c[:, kn_x2].toarray().ravel()*yn[kn_x2 % K]
+                dn2_shift -= t*corr[kn_x, kn_x2]*s[kn_x2]*c[:, kn_x2].toarray().ravel()*yn[kn_x2 % K]
         dn2 += 2 * np.real(alpha * luobj.solve(dn2 + dn2_shift))
     np0 *= c48_alpha0
     us *= c48_alpha0
--- CRAM.py
+++ CRAM.py
@@ -3,12 +3,12 @@ import scipy.sparse as sp
 
 from poles_residues import *
 
-def CRAM(a: sp.csc_array, z0: np.ndarray):
+def CRAM(a: sp.csc_array, z0: np.ndarray, t: float):
     iden = sp.eye(a.shape[0], format = "csc")
 
     y = z0.copy()
     for alpha, theta in zip(c48_alpha, c48_theta):
-        y += 2 * np.real(alpha * sp.linalg.spsolve(a - theta*iden, y))
+        y += 2 * np.real(alpha * sp.linalg.spsolve(t*a - theta*iden, y))
     y *= c48_alpha0
     return y
 
